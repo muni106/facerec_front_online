ba8f468630acf58c21de52643b29796ce5eb229e
Mounir <molt.206@gmail.com>
2020-08-13 11:36:56 +0200

    Updates

---
+++ precache-manifest.779cba024f7b1abe24bfe52bd8763414.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4db5bbd38abe5adfe353cba8a043c69",
+    "url": "/facerec_front_online/index.html"
+  },
+  {
+    "revision": "18306d59618b4317a371",
+    "url": "/facerec_front_online/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c6943cb99e29eb6f31bd",
+    "url": "/facerec_front_online/static/css/main.8628bdc2.chunk.css"
+  },
+  {
+    "revision": "18306d59618b4317a371",
+    "url": "/facerec_front_online/static/js/2.cfec3764.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facerec_front_online/static/js/2.cfec3764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6943cb99e29eb6f31bd",
+    "url": "/facerec_front_online/static/js/main.e9ac85f2.chunk.js"
+  },
+  {
+    "revision": "6bc7af92f01935556ec4",
+    "url": "/facerec_front_online/static/js/runtime-main.b626a97e.js"
+  }
+]);
